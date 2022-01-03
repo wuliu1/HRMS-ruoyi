7d480b87999257e1be739df39a53bf87e108373c
dyliaoc <1083696067@qq.com>
2022-01-03 12:39:21 +0800

    前端页面提交 dyliaoc

--- ruoyi-admin/src/main/java/com/ruoyi/RuoYiApplication.java
+++ ruoyi-admin/src/main/java/com/ruoyi/RuoYiApplication.java
@@ -16,6 +16,6 @@ public class RuoYiApplication
     {
         // System.setProperty("spring.devtools.restart.enabled", "false");
         SpringApplication.run(RuoYiApplication.class, args);
-        System.out.println("(♥◠‿◠)ﾉﾞ  若依启动成功   ლ(´ڡ`ლ)ﾞ");
+        System.out.println("(HRMS-ruoyi 启动成功");
     }
 }
